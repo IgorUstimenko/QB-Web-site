29f864de9b2a052339a9c29083d58729d9ab9f8f
Ustimenko <igorUstimeko@yandex.ru>
2020-03-23 16:06:10 +0300

    Закончил раздел с карточками
    -сделал смену нава на маску

---
+++ js/scrollScript.js
@@ -0,0 +1,18 @@
+window.onscroll = function () {
+    let html = document.documentElement,
+        body = document.body;
+    // Скрипт для замены nav на маску с другими стилями
+
+    let navStyle = document.querySelector('.main-nav').style,
+        maskNavStyle = document.querySelector('.mask-nav').style;
+    if (html.scrollTop > 2020 || body.scrollTop > 2020) {
+        navStyle.display = "none";
+        maskNavStyle.opacity = 1;
+
+    } else {
+        navStyle.display = "flex";
+        maskNavStyle.opacity = 0;
+    }
+
+
+};
