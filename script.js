b59ec8153172be3d6c97ec7764f3950fa94ce626
Ustimenko <igorUstimeko@yandex.ru>
2020-06-05 15:42:50 +0300

    script+podcast+mail

---
+++ script.js
@@ -0,0 +1,93 @@
+// Скрипт для отправки форм
+$(document).ready(function () {
+
+
+	//Заявка о сайте
+	$(".main-form").submit(function () {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function () {
+			$('#thanks-modal').modal();
+			setTimeout(function () {
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	// Подписка на рассылку
+	$(".sending-request").submit(function () {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function () {
+			alert("Thank you!");
+			setTimeout(function () {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	// Подписка на рассылку на телефоне
+	$(".mobile-sub-on-our-sending-form").submit(function () { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function () {
+			alert("Thank you!");
+			setTimeout(function () {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	// Отправка формы о сайте на телефоне
+	$(".main-mobile-form").submit(function () {
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function () {
+			$('#mobile-success-modal').modal();
+			setTimeout(function () {
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+});
+
+
+
+// Скролл до страницы кейсов
+
+const casesPage = document.getElementById('mobile-our-works-page');
+const AgecnyPage = document.getElementById('mobile-blossom-agansy-page');
+let DownArrow = document.getElementById('mobile-down-arrow');
+let GetToKnow = document.getElementById('get-to-know-block');
+
+DownArrow.addEventListener('click', function () {
+	casesPage.scrollIntoView({
+		behavior: "smooth"
+	});
+});
+
+GetToKnow.addEventListener('click', function () {
+	AgecnyPage.scrollIntoView({
+		behavior: "smooth"
+	});
+});
