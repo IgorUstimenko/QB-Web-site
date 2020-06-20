1e432c23d729de3d69213b359b329abddc51ea50
IgorUstimenko <igorustimeko@yandex.ru>
2020-06-21 00:04:22 +0300

    Бэк на кейсы пофикшен пикер
    - Бэк на страницах кейсов
    - Пикер работает
    - О союзе => О нас

--- js/form.js
+++ js/form.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", event => {
+  // Форма
   const form = document.querySelector(".block-form .callback-form");
+  // Обертка поля откуда узнали о нас
   const fromAboutInputWrapper = document.querySelector(
     ".block-form .callback-form .top-row .right-side .form-field.from-about-field .input-wrapper"
   );
+  // Поле откуда узнали о нас
   const fromAboutInput = document.querySelector(
     ".block-form .callback-form .top-row .right-side .form-field.from-about-field .input-wrapper input"
   );
+  // Обертка кнопок
   const fromAboutBtnsContainer = document.querySelector(
     ".block-form .callback-form .top-row .right-side .form-field.from-about-field .btn-set"
   );
+  // Кнопки
   const fromAboutBtns = document.querySelectorAll(
-    ".block-form .callback-form .top-row .right-side .form-field.from-about-field .btn-set button"
+    ".modal-cases-btn"
   );
   const telElements = document.querySelectorAll(".tel-input");
 
@@ -62,46 +67,69 @@ document.addEventListener("DOMContentLoaded", event => {
   });
 
   form.addEventListener("submit", (event) => {});
+
+
+  //Скрипт нижней формы
+
+  // Форма
+  const CaseFrom = document.getElementById("#case_form");
+  // Обертка поля откуда узнали о нас
+  const WhereInputWrapper = document.getElementById("#where_input_wrapper");
+  // Поле откуда узнали о нас
+  const WhereInput = document.getElementById("#where_input");
+  // Обертка кнопок
+  const BtnsWrapper = document.getElementById("#btns_wrapper");
+  // Кнопки
+  const CasesBtns = document.querySelectorAll(
+    ".cases-btn"
+  );
+
+  WhereInput.addEventListener("focus", (event) => {
+    BtnsWrapper.classList.add("hidden");
+  });
+
+  WhereInput.addEventListener("blur", (event) => {
+    if (event.target.value.length > 0) {
+      BtnsWrapper.classList.add("hidden");
+
+      return;
+    }
+
+    BtnsWrapper.classList.remove("hidden");
+  });
+
+
+  CasesBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const isChecked = event.target.classList.contains("active");
+      WhereInput.value = btn.textContent;
+      console.log(WhereInput.value);
+
+
+      CasesBtns.forEach((el) => {
+        el.classList.remove("active");
+      });
+
+      if (!isChecked) {
+        event.target.classList.add("active");
+        WhereInputWrapper.classList.add("hidden");
+
+        return;
+      }
+
+      event.target.classList.remove("active");
+      WhereInputWrapper.classList.remove("hidden");
+      WhereInput.value = '';
+
+    });
+  });
+
+  CaseFrom.addEventListener("submit", (event) => {});
+
+
 });
 
 function telMask(element) {
   const value = element.value.replace(/[^0-9]/g, "");
   element.value = value;
 }
-
-
-
-//   fromAboutBtns.forEach(btn => {
-//     btn.addEventListener("click", event => {
-//       const isChecked = event.target.classList.contains("active");
-//       let formInput = document.querySelector('.whereInput');
-
-//       formInput.value = btn.textContent;
-//       console.log(formInput.value)
-
-//       fromAboutBtns.forEach(el => {
-//         el.classList.remove("active");
-//       });
-
-//       if (!isChecked) {
-//         event.target.classList.add("active");
-//         fromAboutInputWrapper.classList.add("hidden");
-
-//         return;
-//       }
-
-//       event.target.classList.remove("active");
-//       fromAboutInputWrapper.classList.remove("hidden");
-//       formInput.value = '';
-
-
-//     });
-//   });
-
-//   form.addEventListener("submit", event => {});
-// });
-
-// function telMask(element) {
-//   const value = element.value.replace(/[^0-9]/g, "");
-//   element.value = value;
-// }
